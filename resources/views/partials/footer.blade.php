28903831205afd0729c0da2db27ac65633c48784
talha bhatti <qararhanzala@gmail.com>
2024-11-26 15:08:20 +0500

    second commit

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <div class="container-fluid">
+        <div class="row text-muted">
+            <div class="col text-center">
+                <h3>#Free Palastine</h3>
+            </div>
+        </div>
+    </div>
+</footer>
